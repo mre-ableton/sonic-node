138f8871f4e1331d5f59767ae289de2fcab4328d
mre-ableton <marc.resibois@ableton.com>
2016-12-15 11:37:36 +0100

    Update mn-to-try.js

--- js/data/mn-to-try.js
+++ js/data/mn-to-try.js
@@ -1,3 +1,8 @@
-rafale + reotini = { 1,3,4,4,4b,4b } ?
+Possible alteration rule:
+
+rafale + rebotini = either { 1,3,4,I} or { 1,1,4,I} depending on what we choose the bassline to be.
+It comes from https://en.wikipedia.org/wiki/Picardy_third
+
+to find out:
 lars and.. "me,me,me" (trentemoller remix)
 space art
